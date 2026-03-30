aad0b2ff887023fcffaf24b3cca06e2a25110ed7
Naveen N <73241991+RabbitFored@users.noreply.github.com>
2026-03-30 22:33:48 +0530

    Update tdlib_client.dart

--- lib/core/tdlib/tdlib_client.dart
+++ lib/core/tdlib/tdlib_client.dart
@@ -46,7 +46,7 @@ class TdLibClient {
     await td_plugin.TdPlugin.initialize('libtdjson.so');
 
     // Initialize the shared EventSubject isolate
-    await EventSubject.initialize(libPath: 'libtdjson.so');
+    await EventSubject.initialize();
 
     _clientId = tdCreate();
     // Forward updates for our client to the broadcast stream.
